453c1c258135e2a71c6831fe8fbd09f0c47fbab5
WenYiHuanooo <162790849+WenYiHuanooo@users.noreply.github.com>
2024-07-22 15:56:41 +0800

    Update main.cpp

--- exercises/13_class_clone/main.cpp
+++ exercises/13_class_clone/main.cpp
@@ -1,38 +1,46 @@
-#include "../exercise.h"
-
-// READ: 复制构造函数 <https://zh.cppreference.com/w/cpp/language/copy_constructor>
+#include <cassert>
+#include "../exercise.h" 
 
 class DynFibonacci {
     size_t *cache;
     int cached;
 
 public:
-    // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    DynFibonacci(int capacity): cache(new size_t[capacity]), cached(0) {}
 
-    // TODO: 实现复制构造器
-    DynFibonacci(DynFibonacci const &other) = delete;
-
-    // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
-
-    // TODO: 实现正确的缓存优化斐波那契计算
-    size_t get(int i) {
-        for (; false; ++cached) {
-            cache[cached] = cache[cached - 1] + cache[cached - 2];
+    DynFibonacci(DynFibonacci const &other): cache(new size_t[other.cached]), cached(other.cached) {
+        for (int i = 0; i <= cached; ++i) {
+            cache[i] = other.cache[i];
         }
+    }
+
+    // 实现析构器，释放缓存空间
+    ~DynFibonacci() {
+        delete[] cache;
+    }
+
+    // 实现正确的缓存优化斐波那契计算
+    size_t get(int i) {
+        if (i <= cached) {
+            return cache[i];
+        }
+        if (cached < 2) {
+            cache[0] = 0;
+            cache[1] = 1;
+        }
+        for (int j = cached + 1; j <= i; ++j) {
+            cache[j] = cache[j - 1] + cache[j - 2];
+        }
+        cached = i;
         return cache[i];
     }
 
-    // NOTICE: 不要修改这个方法
-    // NOTICE: 名字相同参数也相同，但 const 修饰不同的方法是一对重载方法，可以同时存在
-    //         本质上，方法是隐藏了 this 参数的函数
-    //         const 修饰作用在 this 上，因此它们实际上参数不同
+    // 重载方法，与非const方法区别在于不能修改对象的成员变量
     size_t get(int i) const {
         if (i <= cached) {
             return cache[i];
         }
-        ASSERT(false, "i out of range");
+        assert(false); // 由于是const方法，不能调用ASSERT宏
     }
 };
 
