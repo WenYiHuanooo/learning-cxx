e1f42d0d76256d67a0c35d80ab75b7ddec572325
WenYiHuanooo <162790849+WenYiHuanooo@users.noreply.github.com>
2024-07-22 16:24:54 +0800

    Update main.cpp

--- exercises/18_function_template/main.cpp
+++ exercises/18_function_template/main.cpp
@@ -1,8 +1,10 @@
-﻿#include "../exercise.h"
+#include "../exercise.h"
+#include <cassert>
+#include <cmath> // 引入cmath库，用于处理浮点数比较
 
-// READ: 函数模板 <https://zh.cppreference.com/w/cpp/language/function_template>
-// TODO: 将这个函数模板化
-int plus(int a, int b) {
+// 函数模板化
+template<typename T>
+T plus(T a, T b) {
     return a + b;
 }
 
@@ -10,11 +12,13 @@ int main(int argc, char **argv) {
     ASSERT(plus(1, 2) == 3, "Plus two int");
     ASSERT(plus(1u, 2u) == 3u, "Plus two unsigned int");
 
-    // THINK: 浮点数何时可以判断 ==？何时必须判断差值？
+    // 浮点数比较
     ASSERT(plus(1.25f, 2.5f) == 3.75f, "Plus two float");
     ASSERT(plus(1.25, 2.5) == 3.75, "Plus two double");
-    // TODO: 修改判断条件使测试通过
-    ASSERT(plus(0.1, 0.2) == 0.3, "How to make this pass?");
 
+    // 浮点数加法可能存在精度问题，使用差值比较
+    float sum = plus(0.1f, 0.2f);
+    ASSERT(std::fabs(sum - 0.3f) < 1e-6, "How to make this pass?");
+    
     return 0;
 }
