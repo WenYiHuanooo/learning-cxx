b3219cc37c7570cc93ffcc4e12f941bd7aac36f1
WenYiHuanooo <162790849+WenYiHuanooo@users.noreply.github.com>
2024-07-22 16:27:44 +0800

    Update main.cpp

--- exercises/19_runtime_datatype/main.cpp
+++ exercises/19_runtime_datatype/main.cpp
@@ -1,5 +1,6 @@
-﻿#include "../exercise.h"
+#include "../exercise.h"
 #include <cmath>
+#include <cassert>
 
 enum class DataType {
     Float,
@@ -17,14 +18,22 @@ struct TaggedUnion {
     };
 };
 
-// TODO: 将这个函数模板化用于 sigmoid_dyn
-float sigmoid(float x) {
+// 函数模板化用于 sigmoid
+template<typename T>
+T sigmoid(T x) {
     return 1 / (1 + std::exp(-x));
 }
 
 TaggedUnion sigmoid_dyn(TaggedUnion x) {
     TaggedUnion ans{x.type};
-    // TODO: 根据 type 调用 sigmoid
+    switch (x.type) {
+        case DataType::Float:
+            ans.f = sigmoid(x.f);
+            break;
+        case DataType::Double:
+            ans.d = sigmoid(x.d);
+            break;
+    }
     return ans;
 }
 
@@ -33,13 +42,13 @@ int main(int argc, char **argv) {
     TaggedUnion xf{DataType::Float};
     xf.f = 5.f;
     auto yf = sigmoid_dyn(xf);
-    ASSERT(yf.type == DataType::Float, "type mismatch");
-    ASSERT(yf.f == 1 / (1 + std::exp(-5.f)), "sigmoid float");
+    assert(yf.type == DataType::Float && "type mismatch");
+    assert(yf.f == 1 / (1 + std::exp(-5.f)) && "sigmoid float");
 
     TaggedUnion xd{DataType::Double};
     xd.d = 5.0;
     auto yd = sigmoid_dyn(xd);
-    ASSERT(yd.type == DataType::Double, "type mismatch");
-    ASSERT(yd.d == 1 / (1 + std::exp(-5.0)), "sigmoid double");
+    assert(yd.type == DataType::Double && "type mismatch");
+    assert(yd.d == 1 / (1 + std::exp(-5.0)) && "sigmoid double");
     return 0;
 }
