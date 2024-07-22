aaefd0f6b67c08a93049494a4d4f97e9e7f7d285
WenYiHuanooo <162790849+WenYiHuanooo@users.noreply.github.com>
2024-07-22 13:26:40 +0800

    Update main.cpp

--- exercises/01_variable&add/main.cpp
+++ exercises/01_variable&add/main.cpp
@@ -3,8 +3,10 @@
 // READ: 运算符 <https://zh.cppreference.com/w/cpp/language/expressions#.E8.BF.90.E7.AE.97.E7.AC.A6>
 
 int main(int argc, char **argv) {
-    // TODO: 补全变量定义并打印加法运算
-    // x ?
+    // 定义一个整型变量 x 并初始化，例如初始化为 5
+    int x = 5;
+
+    // 使用 std::cout 输出加法运算的结果
     std::cout << x << " + " << x << " = " << x + x << std::endl;
     return 0;
 }
