21cbb494c9ff22ac485f7710fe6724dbe2e9b33e
WenYiHuanooo <162790849+WenYiHuanooo@users.noreply.github.com>
2024-07-28 16:08:25 +0800

    Update main.cpp

--- exercises/11_class/main.cpp
+++ exercises/11_class/main.cpp
@@ -13,28 +13,21 @@ class Fibonacci {
     int cached;
 
 public:
-    // 实现构造器，初始化 cache 数组和 cached 变量
-    Fibonacci() : cached(-1) {
-        std::fill(cache, std::end(cache), 0); // 初始化 cache 数组为 0
-    }
+    // TODO: 实现构造器
+    Fibonacci():cache{0,1},cached{2} {}
 
+    // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        if (cached < i) {
-            for (int n = std::max(cached + 1, 0); n <= i; ++n) {
-                if (n < 2) {
-                    cache[n] = n; 
-                } else {
-                    cache[n] = cache[n - 1] + cache[n - 2];
-                }
-            }
-            cached = i; 
+        for (; cached < 16; ++cached) {
+            cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
     }
 };
 
 int main(int argc, char **argv) {
-
+    // 现在类型拥有无参构造器，声明时会直接调用。
+    // 这个写法不再是未定义行为了。
     Fibonacci fib;
     ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
