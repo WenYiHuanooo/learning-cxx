67b419368bf93b246e816279fa8e9ee6a156d5f6
WenYiHuanooo <162790849+WenYiHuanooo@users.noreply.github.com>
2024-07-28 15:20:51 +0800

    Update main.cpp

--- exercises/29_std_accumulate/main.cpp
+++ exercises/29_std_accumulate/main.cpp
@@ -1,17 +1,20 @@
-﻿#include "../exercise.h"
+#include "../exercise.h"
 #include <numeric>
-
-// READ: `std::accumulate` <https://zh.cppreference.com/w/cpp/algorithm/accumulate>
+#include <iostream>
 
 int main(int argc, char **argv) {
     using DataType = float;
     int shape[]{1, 3, 224, 224};
-    // TODO: 调用 `std::accumulate` 计算：
-    //       - 数据类型为 float；
-    //       - 形状为 shape；
-    //       - 连续存储；
-    //       的张量占用的字节数
-    // int size =
+    
+    // 使用 std::accumulate 计算张量元素的总数
+    auto total_elements = std::accumulate(shape, shape + 4, 1, std::multiplies<int>());
+    
+    // 计算占用的字节数，float 类型占用 4 个字节
+    int size = total_elements * sizeof(DataType);
+    
+    // 断言检查结果是否正确
     ASSERT(size == 602112, "4x1x3x224x224 = 602112");
+    
+    std::cout << "Size in bytes: " << size << std::endl;
     return 0;
 }
