5f04af2d8576d8b4529aac8e6d36e39ecb9e9a5a
WenYiHuanooo <162790849+WenYiHuanooo@users.noreply.github.com>
2024-07-28 15:29:18 +0800

    Update main.cpp

--- exercises/21_template_const/main.cpp
+++ exercises/21_template_const/main.cpp
@@ -1,7 +1,5 @@
-﻿#include "../exercise.h"
+#include "../exercise.h"
 #include <cstring>
-
-// READ: 模板非类型实参 <https://zh.cppreference.com/w/cpp/language/template_parameters#%E6%A8%A1%E6%9D%BF%E9%9D%9E%E7%B1%BB%E5%9E%8B%E5%AE%9E%E5%8F%82>
 
 template<unsigned int N, class T>
 struct Tensor {
@@ -9,8 +7,13 @@ struct Tensor {
     T *data;
 
     Tensor(unsigned int const shape_[N]) {
+        for (unsigned int i = 0; i < N; ++i) {
+            shape[i] = shape_[i];
+        }
         unsigned int size = 1;
-        // TODO: 填入正确的 shape 并计算 size
+        for (unsigned int i = 0; i < N; ++i) {
+            size *= shape[i];
+        }
         data = new T[size];
         std::memset(data, 0, size * sizeof(T));
     }
@@ -18,7 +21,7 @@ struct Tensor {
         delete[] data;
     }
 
-    // 为了保持简单，禁止复制和移动
+    // 禁止复制和移动
     Tensor(Tensor const &) = delete;
     Tensor(Tensor &&) noexcept = delete;
 
@@ -32,38 +35,12 @@ struct Tensor {
 private:
     unsigned int data_index(unsigned int const indices[N]) const {
         unsigned int index = 0;
-        for (unsigned int i = 0; i < N; ++i) {
+        unsigned int stride = 1;
+        for (int i = N - 1; i >= 0; --i) {
             ASSERT(indices[i] < shape[i]);
-            // TODO: 计算 index
+            index += indices[i] * stride;
+            stride *= shape[i];
         }
+        return index;
     }
 };
-
-// ---- 不要修改以下代码 ----
-int main(int argc, char **argv) {
-    {
-        unsigned int shape[]{2, 3, 4, 5};
-        auto tensor = Tensor<4, int>(shape);
-
-        unsigned int i0[]{0, 0, 0, 0};
-        tensor[i0] = 1;
-        ASSERT(tensor[i0] == 1, "tensor[i0] should be 1");
-
-        unsigned int i1[]{1, 2, 3, 4};
-        tensor[i0] = 2;
-        ASSERT(tensor[i0] == 2, "tensor[i1] should be 2");
-    }
-    {
-        unsigned int shape[]{7, 8, 128};
-        auto tensor = Tensor<3, float>(shape);
-
-        unsigned int i0[]{0, 0, 0};
-        tensor[i0] = 1.f;
-        ASSERT(tensor[i0] == 1.f, "tensor[i0] should be 1");
-
-        unsigned int i1[]{3, 4, 99};
-        tensor[i0] = 2.f;
-        ASSERT(tensor[i0] == 2.f, "tensor[i1] should be 2");
-    }
-    return 0;
-}
