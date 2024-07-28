11927227eee3f793801f5cf598fb586fe506f8b8
WenYiHuanooo <162790849+WenYiHuanooo@users.noreply.github.com>
2024-07-28 15:28:17 +0800

    Update main.cpp

--- exercises/22_std_array/main.cpp
+++ exercises/22_std_array/main.cpp
@@ -1,28 +1,25 @@
-﻿#include "../exercise.h"
+#include "../exercise.h"
 #include <array>
 #include <cstring>
 
-// READ: std::array <https://zh.cppreference.com/w/cpp/container/array>
-
-// TODO: 将下列 `?` 替换为正确的代码
 int main(int argc, char **argv) {
     {
         std::array<int, 5> arr{{1, 2, 3, 4, 5}};
-        ASSERT(arr.size() == ?, "Fill in the correct value.");
-        ASSERT(sizeof(arr) == ?, "Fill in the correct value.");
+        ASSERT(arr.size() == 5, "Fill in the correct value."); // 数组的大小是其元素的数量
+        ASSERT(sizeof(arr) == sizeof(int) * 5, "Fill in the correct value."); // 数组的 sizeof 等于其元素类型大小乘以元素数量
         int ans[]{1, 2, 3, 4, 5};
-        ASSERT(std::memcmp(arr.?, ans, ?) == 0, "Fill in the correct values.");
+        ASSERT(std::memcmp(arr.data(), ans, sizeof(ans)) == 0, "Fill in the correct values."); // 使用 data() 获取数组的原始数据
     }
     {
         std::array<double, 8> arr;
-        ASSERT(arr.size() == ?, "Fill in the correct value.");
-        ASSERT(sizeof(arr) == ?, "Fill in the correct value.");
+        ASSERT(arr.size() == 8, "Fill in the correct value."); // 默认构造的数组，元素未初始化
+        ASSERT(sizeof(arr) == sizeof(double) * 8, "Fill in the correct value."); // 同上
     }
     {
         std::array<char, 21> arr{"Hello, InfiniTensor!"};
-        ASSERT(arr.size() == ?, "Fill in the correct value.");
-        ASSERT(sizeof(arr) == ?, "Fill in the correct value.");
-        ASSERT(std::strcmp(arr.?, "Hello, InfiniTensor!") == 0, "Fill in the correct value.");
+        ASSERT(arr.size() == 21, "Fill in the correct value."); // 包括字符串结束符 '\0'
+        ASSERT(sizeof(arr) == sizeof(char) * 21, "Fill in the correct value."); // 同上
+        ASSERT(std::strcmp(arr.data(), "Hello, InfiniTensor!") == 0, "Fill in the correct value."); // 使用 data() 获取数组的原始数据
     }
     return 0;
 }
