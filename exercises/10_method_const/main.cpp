900fdda29e165076036dec3f0f5c9d5197656416
WenYiHuanooo <162790849+WenYiHuanooo@users.noreply.github.com>
2024-07-28 16:07:57 +0800

    Update main.cpp

--- exercises/10_method_const/main.cpp
+++ exercises/10_method_const/main.cpp
@@ -2,15 +2,14 @@
 
 struct Fibonacci {
     int numbers[11];
-
+    // TODO: 修改方法签名和实现，使测试通过
     int get(int i) const {
-        return numbers[i]; 
+        return numbers[i];
     }
 };
 
 int main(int argc, char **argv) {
-    // 使用 constexpr 声明 FIB 对象，它将在编译时初始化
-    constexpr Fibonacci FIB{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+    Fibonacci constexpr FIB{{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55}};
     ASSERT(FIB.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << FIB.get(10) << std::endl;
     return 0;
